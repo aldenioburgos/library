9aad80488b193a9e4112500122972964a03daecf
aldenio <aldenio@gmail.com>
2020-09-08 20:11:19 -0300

    executable jar

--- local_build.sh
+++ local_build.sh
@@ -1,6 +1,19 @@
 
-javac -verbose  --release 11  -sourcepath ./src -cp ./lib/bft-smart.jar;./dist/BFT-SMaRt-Parallel.jar -d ./build ./src/demo/parallelism/LocalHibridExecution2.java
-jar --create --file ./dist/BFT-SMaRt-Hibrid.jar --verbose --main-class demo/parallelism/LocalHibridExecution2 -C ./build .
+javac  --release 11  -sourcepath ./src -cp ./lib/bft-smart.jar;./dist/BFT-SMaRt-Parallel.jar -d ./build ./src/demo/parallelism/LocalHibridExecution.java
+cp ./lib/*.jar ./build
+cd ./build
+tar xf *.jar
+cd ..
+jar --create --file ./dist/BFT-SMaRt-Hibrid.jar --verbose --main-class demo/parallelism/LocalHibridExecution -C ./build .
 
-
-
+mkdir build
+cp ./lib/*.jar ./build
+cd ./build
+unzip -o bft-smart.jar
+unzip -o commons-codec-1.5.jar
+unzip -o core-0.1.4.jar
+unzip -o netty-all-4.0.36.Final.jar
+unzip -o netty-3.1.1.GA.jar
+unzip -o slf4j-api-1.5.8.jar
+unzip -o slf4j-jdk14-1.5.8.jar
+rm *.jar
--- local_run.sh
+++ local_run.sh
@@ -1,3 +1,3 @@
 set bftLibs=./lib/bft-smart.jar;./lib/commons-codec-1.5.jar;./lib/core-0.1.4.jar;./lib/;./dist/BFT-SMaRt.Parallel.jar;./lib/netty-3.1.1.GA.jar;./lib/netty-all-4.0.36.Final.jar;./lib/slf4j-api-1.5.8.jar;./lib/slf4j-jdk14-1.5.8.jar
 
-java -cp %bftLibs%;./dist/BFT-SMaRt-Hibrid.jar demo.parallelism.LocalHibridExecution2 2 2 1000 10 10 10000000 150
+java -cp %bftLibs%;./dist/BFT-SMaRt-Hibrid.jar demo.parallelism.LocalHibridExecution 2 2 1000 10 10 10000000 150

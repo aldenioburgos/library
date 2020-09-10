d725dd757ad37b300da830d7c6d39d51939fe6a9
aldenio <aldenio@gmail.com>
2020-09-10 16:14:06 -0300

    Many edges sisout.

--- testes/local_run_com_perf_stat.sh
+++ testes/local_run_com_perf_stat.sh
@@ -4,3 +4,4 @@ perf stat -d java -jar ../dist/BFT-smart-parallel-executable.jar 1000 10 6  0 0 
 perf stat -d java -XX:-RestrictContended -jar ../dist/BFT-Hibrid_contended-executable.jar  6 10 1000 0 0 10000000 150
 perf stat -d java -jar ../dist/BFT-Hibrid_contended-executable.jar  6 10 1000 0 0 10000000 150
 perf stat -d java -jar ../dist/BFT-Hibrid_padding-executable.jar  6 10 1000 0 0 10000000 150
+perf stat -d java -jar ../dist/BFT-Hibrid_many_edges-executable.jar  6 10 1000 0 0 10000000 150

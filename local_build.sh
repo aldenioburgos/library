fb49ae24eb97e61f8e5b901ce18defb2497433e4
aldenio <aldenio@gmail.com>
2020-09-14 14:55:50 -0300

    Teste com throws para garantir g0w0.

--- local_build.sh
+++ local_build.sh
@@ -1,19 +1,7 @@
 nome=BFT-Hibrid-with-lists-executable.jar
 
 rm -rf build
-mkdir build
-cp ./lib/*.jar ./build
-cd ./build
-unzip -o bft-smart.jar
-unzip -o commons-codec-1.5.jar
-unzip -o core-0.1.4.jar
-unzip -o netty-all-4.0.36.Final.jar
-unzip -o netty-3.1.1.GA.jar
-unzip -o slf4j-api-1.5.8.jar
-unzip -o slf4j-jdk14-1.5.8.jar
-rm *.jar
-rm -rf META-INF
-cd ..
+cp ../build ./build
 javac  -sourcepath ./src -cp ./lib/bft-smart.jar -d ./build ./src/demo/parallelism/LocalHibridExecution.java
 jar cvfe $nome demo.parallelism.LocalHibridExecution -C build .
 mv  $nome ./dist/

6764cbf02a15710558e4dfeb57c093fc8299c642
aldenio <aldenio@gmail.com>
2021-02-11 11:55:33 -0300

    Limpeza geral

--- src/demo/hibrid/server/HibridReplier.java
+++
@@ -1,5 +0,0 @@
-package demo.hibrid.server;
-
-public interface HibridReplier {
-    void manageReply(CommandEnvelope commandEnvelope, boolean[] results) ;
-}
--- testes/local_run_os_tres_com_perf_stat.sh
+++
@@ -1,4 +0,0 @@
-perf stat -d java -jar ../dist/BFT-Hibrid_with_status-executable.jar 6 10 1000 0 0 10000000 150
-perf stat -d java -jar ../dist/BFT-smart-parallel-executable.jar 1000 10 6  0 0 10000000 false 150
-perf stat -d java -jar ../dist/BFT-smart-parallel-executable.jar 1000 10 6  0 0 10000000 true 150
-perf stat -d java -XX:-RestrictContended -jar ../dist/BFT-Hibrid_contended-executable.jar 6 10 1000 0 0 10000000 150

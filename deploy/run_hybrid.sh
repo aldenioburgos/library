d8c647d61c42906944cb7bf30d9bb05f0556e6e7
aldenio <aldenio@gmail.com>
2021-04-10 14:58:20 -0300

    versão inicial do script de testes.

--- deploy/run_hybrid.sh
+++ deploy/run_hybrid.sh
@@ -17,19 +17,19 @@ for w in "${workloads[@]}" ; do
     for LATE_WORKERS_PER_PARTITION in "${threads[@]}" ; do
 
       # executa experimento hybrid:
-      ssh  replica0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}_.bin" &
+      ssh  replica0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}.bin" &
       sleep 3s
-      ssh  replica1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 1 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}_.bin" &
+      ssh  replica1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 1 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}.bin" &
       sleep 3s
-      ssh  replica2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 2 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}_.bin" &
+      ssh  replica2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 2 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}.bin" &
       sleep 30s
-      ssh  cliente0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      ssh  cliente0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}.bin" &
       sleep 3s
-      ssh  cliente1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      ssh  cliente1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}.bin" &
       sleep 3s
-      ssh  cliente2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      ssh  cliente2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}.bin" &
       sleep 3s
-      ssh  cliente3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      ssh  cliente3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}.bin" &
       sleep 4m
       ssh  cliente0  "pkill -f 'java.*bft-smart*'" &
       echo 'cliente0 killed'
--- deploy/setup_java.sh
+++ deploy/setup_java.sh
@@ -2,7 +2,7 @@
 
 nodes=(replica0 replica1 replica2 cliente0 cliente1 cliente2 cliente3)
 for n in "${nodes[@]}" ; do
-  echo ssh  ${n}  "sudo ln -s /users/aldenio/jdk-15.0.2/bin/jar /usr/bin/jar; sudo ln -s /users/aldenio/jdk-15.0.2/bin/java /usr/bin/java; sudo ln -s /users/aldenio/jdk-15.0.2/bin/javac /usr/bin/javac; java -version"
+  ssh  ${n}  "sudo ln -s /users/aldenio/jdk-15.0.2/bin/jar /usr/bin/jar; sudo ln -s /users/aldenio/jdk-15.0.2/bin/java /usr/bin/java; sudo ln -s /users/aldenio/jdk-15.0.2/bin/javac /usr/bin/javac; java -version"
 done;
 echo 'finished setup'
 

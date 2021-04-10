5c5f9bfb81ed8fd922f97ae0f01d8a945738510b
aldenio <aldenio@gmail.com>
2021-04-10 14:55:41 -0300

    versão inicial do script de testes.

--- deploy/run_hybrid.sh
+++ deploy/run_hybrid.sh
@@ -1,22 +1,8 @@
-# Copyright (c) 2018-2019 Eliã Batista
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/bin/bash
 
-#particoes=(1 2 4 6 8)
-#threads=(1 2 4 6 8 10 12 14 16 18 20 24 32 56 64)
 particoes=(1 2 4 6 8)
 threads=(2 4 6)
-workloads=('0 0' '5 10') #perc_global perc_write
+workloads=('0 0' '5 10')
 Server="CoinHybridServiceReplica"
 Client="CoinClient"
 NUM_THREADS_CLIENTE=250
@@ -30,54 +16,44 @@ for w in "${workloads[@]}" ; do
   for p in "${particoes[@]}" ; do
     for LATE_WORKERS_PER_PARTITION in "${threads[@]}" ; do
 
-# executa experimento hybrid:
-ssh  replica0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION}" &
-sleep 3s
-ssh  replica1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 1 ${LATE_WORKERS_PER_PARTITION}" &
-sleep 3s
-ssh  replica2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 2 ${LATE_WORKERS_PER_PARTITION}" &
-sleep 3s
-ssh  cliente0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
-sleep 3s
-ssh  cliente1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
-sleep 3s
-ssh  cliente2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
-sleep 3s
-ssh  cliente3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
-sleep 3m
-ssh   replica2 "pkill -f 'java.*bft-smart*'"
-echo 'replica2 killed'
-ssh  replica1 "pkill -f 'java.*bft-smart*'"
-echo 'replica1 killed'
-ssh  replica0 "pkill -f 'java.*bft-smart*'"
-echo 'replica0 killed'
-ssh  cliente0  "pkill -f 'java.*bft-smart*'"
-echo 'cliente0 killed'
-ssh  cliente1  "pkill -f 'java.*bft-smart*'"
-echo 'cliente1 killed'
-ssh  cliente2  "pkill -f 'java.*bft-smart*'"
-echo 'cliente2 killed'
-ssh  cliente3  "pkill -f 'java.*bft-smart*'"
-echo 'cliente3 killed'
-echo 'finished hibrid'
-sleep 3s
+      # executa experimento hybrid:
+      ssh  replica0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}_.bin" &
+      sleep 3s
+      ssh  replica1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 1 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}_.bin" &
+      sleep 3s
+      ssh  replica2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 2 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}_.bin" &
+      sleep 30s
+      ssh  cliente0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      sleep 3s
+      ssh  cliente1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      sleep 3s
+      ssh  cliente2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      sleep 3s
+      ssh  cliente3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} ${NUM_THREADS_CLIENTE} ${NUM_OPERACOES_PER_CLIENTE} ${NUM_OPS_PER_REQ} ${w} warmup_p${p}_.bin" &
+      sleep 4m
+      ssh  cliente0  "pkill -f 'java.*bft-smart*'" &
+      echo 'cliente0 killed'
+      ssh  cliente1  "pkill -f 'java.*bft-smart*'" &
+      echo 'cliente1 killed'
+      ssh  cliente2  "pkill -f 'java.*bft-smart*'" &
+      echo 'cliente2 killed'
+      ssh  cliente3  "pkill -f 'java.*bft-smart*'" &
+      echo 'cliente3 killed'
+      ssh  replica2 "pkill -f 'java.*bft-smart*'" &
+      echo 'replica2 killed'
+      ssh  replica1 "pkill -f 'java.*bft-smart*'" &
+      echo 'replica1 killed'
+      ssh  replica0 "pkill -f 'java.*bft-smart*'"
+      echo 'replica0 killed'
+      echo
+      sleep 3s
+      echo 'finished hibrid'
 
-	#params: 'w-w-w-shards-threads-size-skewed-experiment-replicas-generateReport-totalExp-execDuration-clients-copyFiles generateConsolidatedFiles-warmup'
-	#ssh  elia@replica0 "cd workstealing; java -classpath psmr.jar bftsmart.util.ConsolidateFiles '${w}-${p}-${t}-${s}-${skew}-1-3-false-1-${intervalMinutes}-${clients}-true-false-${warmup}'"
-#sleep 1s
-#echo 'files copied to experiment directory'
+    done;
+  done;
 
-#ssh  elia@replica0 "pkill -f 'java.*bft-smart*'"
-#echo 'replica0 killed'
-#sleep 5s
-
-#done;
-
-done;
-done;
-
-echo mkdir "hybrid_${w}"
-mv results* "hybrid_${w}"
+  echo mkdir "hybrid_${w}"
+  mv results* "hybrid_${w}"
 
 done;
 
--- deploy/setup_java.sh
+++ deploy/setup_java.sh
@@ -1,11 +1,9 @@
+#!/bin/bash
 
-nodes=('replica0' 'replica1' 'replica2' 'cliente0' 'cliente1' 'cliente2' 'cliente3')
-
+nodes=(replica0 replica1 replica2 cliente0 cliente1 cliente2 cliente3)
 for n in "${nodes[@]}" ; do
-  echo ssh  ${n}  "sudo ln -s /users/aldenio/jdk-15.0.2/bin/jar /usr/bin/jar; sudo ln -s /users/aldenio/jdk-15.0.2/bin/java /usr/bin/java; sudo ln -s /users/aldenio/jdk-15.0.2/bin/javac /usr/bin/javac"
-  echo ssh  ${n}  "java -version > ./${n}.txt"
+  echo ssh  ${n}  "sudo ln -s /users/aldenio/jdk-15.0.2/bin/jar /usr/bin/jar; sudo ln -s /users/aldenio/jdk-15.0.2/bin/java /usr/bin/java; sudo ln -s /users/aldenio/jdk-15.0.2/bin/javac /usr/bin/javac; java -version"
 done;
-
 echo 'finished setup'
 
 

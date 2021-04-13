39a5b5523c6e06d848edd6c781330d94e4005e6f
aldenio de vilaca burgos <aldenio@localhost>
2021-04-13 08:08:28 -0600

    arquivos gerados

--- deploy/run_hybrid.sh
+++ deploy/run_hybrid.sh
@@ -13,33 +13,33 @@ for w in "${workloads[@]}" ; do
     for LATE_WORKERS_PER_PARTITION in "${threads[@]}" ; do
 
       # executa experimento hybrid:
-      ssh  replica0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}.bin" &
+      ssh  replica0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin 2>&1 ./logs/log_replica0.txt" &
       sleep 3s
-      ssh  replica1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 1 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}.bin" &
+      ssh  replica1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 1 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin 2>&1 ./logs/log_replica1.txt" &
       sleep 3s
-      ssh  replica2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 2 ${LATE_WORKERS_PER_PARTITION} warmup_p${p}.bin" &
+      ssh  replica2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 2 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin 2>&1 ./logs/log_replica2.txt" &
       sleep 30s
-      ssh  cliente0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 0 ${NUM_THREADS_CLIENTE} ${w} warmup_p${p}.bin" &
+      ssh  cliente0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 0 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin 2>&1 ./logs/log_cliente0.txt" &
       sleep 3s
-      ssh  cliente1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 1 ${NUM_THREADS_CLIENTE} ${w} warmup_p${p}.bin" &
+      ssh  cliente1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 1 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin 2>&1 ./logs/log_cliente1.txt" &
       sleep 3s
-      ssh  cliente2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 2 ${NUM_THREADS_CLIENTE} ${w} warmup_p${p}.bin" &
+      ssh  cliente2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 2 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin 2>&1 ./logs/log_cliente2.txt" &
       sleep 3s
-      ssh  cliente3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 3 ${NUM_THREADS_CLIENTE} ${w} warmup_p${p}.bin" &
+      ssh  cliente3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 3 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin 2>&1 ./logs/log_cliente3.txt" &
       sleep 4m
-      ssh  cliente0  "pkill -f 'java.*bft-smart*'" &
+      ssh  cliente0  "pkill -f '*java*'" &
       echo 'cliente0 killed'
-      ssh  cliente1  "pkill -f 'java.*bft-smart*'" &
+      ssh  cliente1  "pkill -f '*java*'" &
       echo 'cliente1 killed'
-      ssh  cliente2  "pkill -f 'java.*bft-smart*'" &
+      ssh  cliente2  "pkill -f '*java*'" &
       echo 'cliente2 killed'
-      ssh  cliente3  "pkill -f 'java.*bft-smart*'" &
+      ssh  cliente3  "pkill -f '*java*'" &
       echo 'cliente3 killed'
-      ssh  replica2 "pkill -f 'java.*bft-smart*'" &
+      ssh  replica2 "pkill -f '*java*'" &
       echo 'replica2 killed'
-      ssh  replica1 "pkill -f 'java.*bft-smart*'" &
+      ssh  replica1 "pkill -f '*java*'" &
       echo 'replica1 killed'
-      ssh  replica0 "pkill -f 'java.*bft-smart*'"
+      ssh  replica0 "pkill -f '*java*'" &
       echo 'replica0 killed'
       echo
       sleep 3s

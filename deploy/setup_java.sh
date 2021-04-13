361aa245869be7080800e6d002f032a5c6a29617
aldenio <aldenio@gmail.com>
2021-04-12 21:33:21 -0300

    Movi o proxy para fora da thread cliente, agora estão compartilhando.

--- deploy/setup_java.sh
+++ deploy/setup_java.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo ln -s /users/aldenio/jdk-15.0.2/bin/jar /usr/bin/jar; sudo ln -s /users/aldenio/jdk-15.0.2/bin/java /usr/bin/java; sudo ln -s /users/aldenio/jdk-15.0.2/bin/javac /usr/bin/javac; java -version
+
 nodes=(replica0 replica1 replica2 cliente0 cliente1 cliente2 cliente3)
 for n in "${nodes[@]}" ; do
   ssh  ${n}  "sudo ln -s /users/aldenio/jdk-15.0.2/bin/jar /usr/bin/jar; sudo ln -s /users/aldenio/jdk-15.0.2/bin/java /usr/bin/java; sudo ln -s /users/aldenio/jdk-15.0.2/bin/javac /usr/bin/javac; java -version"

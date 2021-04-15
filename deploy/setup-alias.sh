a27b8d6b4e24ee3b09118fdb7bd017c8ef85a149
aldenio <aldenio@gmail.com>
2021-04-15 11:52:00 -0300

    novos scripts

--- deploy/setup-alias.sh
+++ deploy/setup-alias.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 alias l ll
-alias g git status
-alias ga git add
-alias gc git commit -a
-alias gp git push https://aldenioburgos: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@github.com/aldenioburgos/library.git
-alias run ./run_hybrid.sh >& log & ; tail -f log
-alias top top -ualdenio
-alias stop pkill run_hybrid
-alias kill ./kill_hybrid.sh
+alias g "git status"
+alias ga "git add"
+alias gc "git commit -a"
+alias gp "git push https://aldenioburgos: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@github.com/aldenioburgos/library.git"
+alias run "./run_hybrid.sh >& log & ; tail -f log"
+alias top "top -ualdenio"
+alias stop "pkill run_hybrid"
+alias kill "./kill_hybrid.sh"
 
 nodes=(0 1 2 3)
 for n in {0..3} ; do
-  alias r${n} ssh replica${n}
-  alias c${n} ssh cliente${n}
+  alias r${n} "ssh replica${n}"
+  alias c${n} "ssh cliente${n}"
 done;
 
 echo 'installed aliases'

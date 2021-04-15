bdded8c266e09300d6c4a2531fe48591e82b684a
aldenio <aldenio@gmail.com>
2021-04-15 11:47:17 -0300

    novos scripts

--- deploy/setup-alias.sh
+++ deploy/setup-alias.sh
@@ -5,7 +5,7 @@ alias g git status
 alias ga git add
 alias gc git commit -a
 alias gp git push https://aldenioburgos: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@github.com/aldenioburgos/library.git
-
+alias run ./run_hybrid.sh >& log & ; tail -f log
 nodes=(0 1 2 3)
 for n in {0..3} ; do
   alias r${n} ssh replica${n}

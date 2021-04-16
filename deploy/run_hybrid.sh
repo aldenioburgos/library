105aa84a5880b6cd7342fc7c66d67403cce0013c
aldenio <aldenio@gmail.com>
2021-04-16 15:01:12 -0300

    novo script

--- deploy/run_hybrid.sh
+++ deploy/run_hybrid.sh
@@ -4,75 +4,67 @@ NUM_THREADS_CLIENTE=1000
 particoes=(2 4 6 8)
 threads=(2 4 6)
 workloads=('0 0' '5 10')  #percGlobal #percWrite
-#workloads=('5 10')  #percGlobal #percWrite
 Server="CoinHybridServiceReplica"
 Client="CoinClient"
 contadorDeWorkload=0
 contadorDeExecucao=0
-
 for w in "${workloads[@]}" ; do
   for p in "${particoes[@]}" ; do
     for LATE_WORKERS_PER_PARTITION in "${threads[@]}" ; do
       # servidores
-      ssh  replica0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin >& /local/logs/log_r0-e${contadorDeExecucao}.txt" &
-      echo created replica0
-      ssh  replica1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 1 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin >& /local/logs/log_r1-e${contadorDeExecucao}.txt" &
-      echo created replica1
-      ssh  replica2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 2 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin >& /local/logs/log_r2-e${contadorDeExecucao}.txt" &
-      echo created replica2
-      ssh  replica3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 3 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin >& /local/logs/log_r3-e${contadorDeExecucao}.txt" &
-      echo created replica3
-      # espera as replicas sincronizarem
+      echo starting replicas
+      for i in {0..3} ; do
+        ssh  replica${i}  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Server} 0 ${LATE_WORKERS_PER_PARTITION} ./warmup/warmup_p${p}.bin >& /local/logs/log_r${i}-e${contadorDeExecucao}.txt" &
+        echo created replica${i}
+      done
       sleep 30s
-      # clientes
       echo starting clients
-      ssh  cliente0  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 0 4001 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin >& /local/logs/log_c0-e${contadorDeExecucao}.txt" &
-      echo created cliente0
-      ssh  cliente1  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 1 5001 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin >& /local/logs/log_c1-e${contadorDeExecucao}.txt" &
-      echo created cliente1
-      ssh  cliente2  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 2 6001 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin >& /local/logs/log_c2-e${contadorDeExecucao}.txt" &
-      echo created cliente2
-      ssh  cliente3  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 3 7001 ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin >& /local/logs/log_c3-e${contadorDeExecucao}.txt" &
-      echo created cliente3
+      for i in {0..3} ; do
+        ssh  cliente${i}  "cd ~/hybridpsmr/deploy; java -classpath psmr.jar demo.coin.${Client} 0 $((4001 + (i*1000))) ${NUM_THREADS_CLIENTE} ${w} ./warmup/warmup_p${p}.bin >& /local/logs/log_c${i}-e${contadorDeExecucao}.txt" &
+        echo created cliente${i}
+        sleep 3s
+      done
       # tempo de execução
       sleep 1m
       echo 1 minute
       sleep 1m
       echo 2 minutes
       sleep 1m
-      echo 3 minutes
-      sleep 1m
-      echo encerrou
+      echo 3 minutes encerrou
       # mata tudo para começar denovo
-      ssh  cliente0  "pkill -f java" &
-      echo 'cliente0 killed'
-      ssh  cliente1  "pkill -f java" &
-      echo 'cliente1 killed'
-      ssh  cliente2  "pkill -f java" &
-      echo 'cliente2 killed'
-      ssh  cliente3  "pkill -f java" &
-      echo 'cliente3 killed'
-      ssh  replica2 "pkill -f java" &
-      echo 'replica2 killed'
-      ssh  replica1 "pkill -f java" &
-      echo 'replica1 killed'
-      ssh  replica0 "pkill -f java" &
-      echo 'replica0 killed'
-      echo
-      sleep 3s
+      for i in {0..3} ; do
+        ssh  cliente${i}  "pkill -f java" &
+        echo cliente${i} killed
+        sleep 3s
+      done
+      for i in {0..3} ; do
+        ssh  replica${i}  "pkill -f java" &
+        echo replica${i} killed
+        sleep 3s
+      done
+      sleep 10s
       echo finished hibrid execution ${contadorDeExecucao}
       contadorDeExecucao=$((contadorDeExecucao + 1))
+      echo
     done;
   done;
+  agora=`date +"%y-%m-%d-%T"`
+  resultsDir=results_hybrid_coin_${contadorDeWorkload}_${agora}
+  echo movendo os resultados para ${resultsDir}
+  mkdir ${resultsDir}
+  mv resultsCoin* ${resultsDir}
 
-  mkdir "results_hybrid_coin_${contadorDeWorkload}"
-  mv resultsC* "results_hybrid_coin_${contadorDeWorkload}"
+  logsDir=logs_${agora}
+  echo movendo os logs para ${logsDir}
+  mkdir ${logsDir}
+  for i in {0..3} ; do
+      ssh  cliente${i}  "tar -czf ~/hybridpsmr/deploy/${logsDir}/log_c${i}.tar.gz /local/logs/*" &
+      ssh  replica${i}  "tar -czf ~/hybridpsmr/deploy/${logsDir}/log_r${i}.tar.gz /local/logs/*" &
+  done
   contadorDeWorkload=$((contadorDeWorkload + 1))
-
 done;
-
-
-
 echo 'finished all'
+
+
 
 

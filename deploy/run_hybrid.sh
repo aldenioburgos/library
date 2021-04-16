1299e645b00ee0637a9b18172553cf155854df4f
aldenio <aldenio@gmail.com>
2021-04-16 10:07:07 -0300

    correção do script

--- deploy/run_hybrid.sh
+++ deploy/run_hybrid.sh
@@ -3,8 +3,8 @@
 NUM_THREADS_CLIENTE=1000
 particoes=(2 4 6 8)
 threads=(2 4 6)
-#workloads=('0 0' '5 10')  #percGlobal #percWrite
-workloads=('5 10')  #percGlobal #percWrite
+workloads=('0 0' '5 10')  #percGlobal #percWrite
+#workloads=('5 10')  #percGlobal #percWrite
 Server="CoinHybridServiceReplica"
 Client="CoinClient"
 contadorDeWorkload=0
